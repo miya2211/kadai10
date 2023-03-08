83739db02fa35c6731629ecff0f46d08b9386ad5
Miyano Ami <a08018656523@icloud.com>
2023-03-08 12:59:55 +0900

    不要なメソッドを削除。

--- src/main/java/com/example/demo/service/ResourceNotFoundException.java
+++ src/main/java/com/example/demo/service/ResourceNotFoundException.java
@@ -2,20 +2,8 @@ package com.example.demo.service;
 
 public class ResourceNotFoundException extends RuntimeException {
 
-    public ResourceNotFoundException() {
-        super();
-    }
-
-    public ResourceNotFoundException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
     public ResourceNotFoundException(String message) {
         super(message);
-    }
-
-    public ResourceNotFoundException(Throwable cause) {
-        super(cause);
     }
 
 }
