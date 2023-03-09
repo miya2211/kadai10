d094d58ce861bf1a60ce6f36e62a7512dc74d281
Miyano Ami <a08018656523@icloud.com>
2023-03-09 20:37:02 +0900

    必要なメソッドだけ記述に変更。

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,13 +1,23 @@
 package com.example.demo.entity;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 
-@Data
 @AllArgsConstructor
 public class User {
 
     private int id;
     private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
 }
